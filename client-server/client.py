66de8b6f3ff8225e22e3af65ecea05f9b2fb693a
andyfernandes <andrezafernandes@alu.ufc.br>
2019-08-13 15:22:48 -0300

    [+] Conexão TCP funcionando o envio de msg do server pro client

--- client-server/client.py
+++ client-server/client.py
@@ -4,18 +4,18 @@ ip = socket.gethostbyname(socket.gethostname())
 port = input("Port: ")
 addr = ((ip, int(port)))
 
-# Instancia o SOCKET
-# 1o PARAMETRO: socket.AF_INET: Familia do protocolo
-# 2o PARAMETRO: socket.SOCK_STREAM: Inidica que é TCP/IP
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect(addr)
 
 while True:
-    msg = input('\nWhat are you want?\nUPTIME: Time the server is active\nREQNUM: Number of requests\nCLOSE: Finish connection\n: ')
+    msg = raw_input('\nWhat are you want?\nUPTIME: Time the server is active\nREQNUM: Number of requests\nCLOSE: Finish connection\n: ')
     client_socket.send(msg.encode())
     print('\nMessage sent!')
     if(msg == 'CLOSE'):
         print('Finishing connection...')
         break
+    print("\nRESPONSE: \n")
+    newMsg = client_socket.recv(1024)
+    print(newMsg)
 client_socket.close()
 
--- client-server/server.py
+++ client-server/server.py
@@ -10,15 +10,9 @@ host = socket.gethostbyname(socket.gethostname())
 port = input("Port: ")
 addr = (host, int(port))
 
-# Instancia o SOCKET
-# 1o PARAMETRO: socket.AF_INET: Familia do protocolo
-# 2o PARAMETRO: socket.SOCK_STREAM: Inidica que é TCP/IP
 serv_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# zerar o TIME_WAIT do Socket, por exemplo, se o programa estiver aguardando uma conexão e você der CTRL+C para interromper, o programa  será fechado, porém o Socket continua na escuta
 serv_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-# qual IP e porta o servidor deve aguardar a conexão
 serv_socket.bind(addr)
-# Limite de conexões
 serv_socket.listen(10)
 
 while True:
@@ -29,14 +23,13 @@ while True:
     print("\nConnected with ", cliente)
     
     while openSocket:
-        # Aguarda um dado enviado pela rede de até 1024 Bytes - Tamanho do Buffer de recepção
         msg = con.recv(1024)
         print("\nMessage: ", msg.decode())
         
         if(msg.decode() == 'UPTIME'):
-            print("Current Time = ", (datetime.now() - start))
+            con.send(("Current Time = " + str(datetime.now() - start)).encode())
         elif(msg.decode() == 'REQNUM'):
-            print("Connectios = ", contRequests)
+            con.send(("Connectios = " + str(contRequests)).encode())
         elif(msg.decode() == 'CLOSE'):
             print('Finishing connection...\n')
             openSocket = False
